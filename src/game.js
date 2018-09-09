efb4b8fc667a9139ac5c288ca3db1028a01da936
tks <t.schuhmacher@gmail.com>
2018-09-09 22:30:44 +0200

    relative paths added

--- src/game.js
+++ src/game.js
@@ -1,22 +1,20 @@
 /*
 -_-_-_-_-_-_-_-_-_- INIT KONTRA ENGINE _-_-_-_-_-_-_-_-_-
 */
-kontra.init()
+kontra.init();
 
 //loading the assets first and then starting the game
-kontra.assets.imagePath = '/Users/asisa/Projects/13kGame/src/img/';
-kontra.assets.load('yingyang.png','rgb-pixel.png','blue.png','red.png', 'yellow.png', 'dog.png', 'dog-blue.png','dog-yellow.png', 'dog-red.png')
-.then( function()
-  {
+kontra.assets.imagePath = 'src/img';
+kontra.assets.load('rgb-pixel.png', 'blue.png', 'red.png', 'yellow.png', 'dog.png', 'dog-blue.png', 'dog-yellow.png', 'dog-red.png')
+.then(function() {
+    var score = 0;
 
-  var score = 0;
-
-/*
--_-_-_-_-_-_-_-_-_- Sprites and Image Loader _-_-_-_-_-_-_-_-_-
-*/
-//BACKGROUND
-  let background = new Image();
-  background.src = '/Users/asisa/Projects/13kGame/src/img/rgb-pixel.png';
+    /*
+    -_-_-_-_-_-_-_-_-_- Sprites and Image Loader _-_-_-_-_-_-_-_-_-
+    */
+    //BACKGROUND
+    let background = new Image();
+    background.src = 'src/img/rgb-pixel.png';
 
     var backgroundSprite = kontra.sprite({
       x: 0,
@@ -32,15 +30,15 @@ kontra.assets.load('yingyang.png','rgb-pixel.png','blue.png','red.png', 'yellow.
       dy: 1
     });
 
-//THE MAIN PLAYER THE DOG
+    //THE MAIN PLAYER THE DOG
     let dogimg = new Image();
-    dogimg.src = '/Users/asisa/Projects/13kGame/src/img/dog.png';
+    dogimg.src = 'src/img/dog.png';
     let dogredimg = new Image();
-    dogredimg.src = '/Users/asisa/Projects/13kGame/src/img/dog-red.png';
+    dogredimg.src = 'src/img/dog-red.png';
     let dogblueimg = new Image();
-    dogblueimg.src = '/Users/asisa/Projects/13kGame/src/img/dog-blue.png';
+    dogblueimg.src = 'src/img/dog-blue.png';
     let dogyellowimg = new Image();
-    dogyellowimg.src = '/Users/asisa/Projects/13kGame/src/img/dog-yellow.png';
+    dogyellowimg.src = 'src/img/dog-yellow.png';
     var player = kontra.sprite({
         x: 120,
         y: 200,
@@ -48,10 +46,10 @@ kontra.assets.load('yingyang.png','rgb-pixel.png','blue.png','red.png', 'yellow.
         dy: 0
       });
 
-//DOTS FOR CHANGING THE COLOR
+    //DOTS FOR CHANGING THE COLOR
 
     let redimg = new Image();
-    redimg.src = '/Users/asisa/Projects/13kGame/src/img/red.png';
+    redimg.src = 'src/img/red.png';
     var red = kontra.sprite({
         x: 100,
         y: -1,
@@ -60,7 +58,7 @@ kontra.assets.load('yingyang.png','rgb-pixel.png','blue.png','red.png', 'yellow.
       });
 
     let yellowimg = new Image();
-    yellowimg.src = '/Users/asisa/Projects/13kGame/src/img/yellow.png';
+    yellowimg.src = 'src/img/yellow.png';
     var yellow = kontra.sprite({
         x: 50,
         y: -50,
@@ -69,119 +67,115 @@ kontra.assets.load('yingyang.png','rgb-pixel.png','blue.png','red.png', 'yellow.
       });
 
     let blueimg = new Image();
-    blueimg.src = '/Users/asisa/Projects/13kGame/src/img/blue.png';
+    blueimg.src = 'src/img/blue.png';
     var blue = kontra.sprite({
         x: 220,
-        y: - 200,
+        y: -200,
         image: blueimg,
         dy: 1.5
       });
 
+    //ARRAY WITH ITEMS TO COLLECT
 
+    var items = [
 
-//ARRAY WITH ITEMS TO COLLECT
-
-
-      var items = [
-
-        kontra.sprite({
+      kontra.sprite({
         x: 100,
         y: 0,
-        width:10,
-        height:5,
+        width: 10,
+        height: 5,
         color: 'red',
         dy: 1,
       }),
 
       kontra.sprite({
-      x: 30,
-      y: -100,
-      width:10,
-      height:5,
-      color: 'red',
-      dy: 1,
-    }),
-
-        kontra.sprite({
-        x: 150,
-        y: -200,
-        width:10,
-        height:5,
-        color: 'blue',
-        dy: 1.5
-        }),
+        x: 30,
+        y: -100,
+        width: 10,
+        height: 5,
+        color: 'red',
+        dy: 1,
+      }),
 
       kontra.sprite({
-      x: 150,
-      y: -200,
-      width:10,
-      height:5,
-      color: 'blue',
-      dy: 1.5
+        x: 150,
+        y: -200,
+        width: 10,
+        height: 5,
+        color: 'blue',
+        dy: 1.5
+      }),
+
+      kontra.sprite({
+        x: 150,
+        y: -200,
+        width: 10,
+        height: 5,
+        color: 'blue',
+        dy: 1.5
       }),
 
         kontra.sprite({
-        x: 30,
-        y: -10,
-        width:10,
-        height:5,
-        color: 'yellow',
-        dy: 0.5
+          x: 30,
+          y: -10,
+          width: 10,
+          height: 5,
+          color: 'yellow',
+          dy: 0.5
         }),
 
         kontra.sprite({
-        x: 30,
-        y: -10,
-        width:10,
-        height:5,
-        color: 'yellow',
-        dy: 0.5
+          x: 30,
+          y: -10,
+          width: 10,
+          height: 5,
+          color: 'yellow',
+          dy: 0.5
         })
 
       ];
-  /*
-  -_-_-_-_-_-_-_-_-_- GAME LOOP _-_-_-_-_-_-_-_-_-
-  */
-  var loop = kontra.gameLoop({
+
+    /*
+    -_-_-_-_-_-_-_-_-_- GAME LOOP _-_-_-_-_-_-_-_-_-
+    */
+    var loop = kontra.gameLoop({
       update: function() {
 
+        /*
+        -_-_-_-_-_-_-_-_-_- CONTROLES _-_-_-_-_-_-_-_-_-
+        */
+        // control player with the keyboard
+
+        if (kontra.keys.pressed('up')) {
+          player.y -= 1;
+        }
+
+        if (kontra.keys.pressed('down')) {
+          player.y += 1;
+        }
+
+        if (kontra.keys.pressed('right')) {
+          player.x += 1;
+        }
+
+        if (kontra.keys.pressed('left')) {
+          player.x -= 1;
+        }
+
+        /*    if(player.y <= 40) {
+          //pause game
+          loop.stop();
+
+          alert('YouWon!');
+        }
+        */
 
 
-  /*
-  -_-_-_-_-_-_-_-_-_- CONTROLES _-_-_-_-_-_-_-_-_-
-  */
-// control player with the keyboard
+        /*
+        -_-_-_-_-_-_-_-_-_- COLLISION _-_-_-_-_-_-_-_-_-
+        */
 
-      if (kontra.keys.pressed('up')) {
-        player.y -= 1;
-      }
-
-      if (kontra.keys.pressed('down')){
-        player.y += 1;
-      }
-
-      if (kontra.keys.pressed('right')){
-        player.x += 1;
-      }
-
-      if (kontra.keys.pressed('left')){
-        player.x -= 1;
-      }
-
-  /*    if(player.y <= 40) {
-        //pause game
-        loop.stop();
-
-        alert('YouWon!');
-      }
-*/
-
-
-/*
--_-_-_-_-_-_-_-_-_- COLLISION _-_-_-_-_-_-_-_-_-
-*/
-
-//Bouncing the sprite on the edgers of the canvas
+        //Bouncing the sprite on the edgers of the canvas
 
         if (player.x >= 320) {
           player.x = 1;
@@ -199,27 +193,24 @@ kontra.assets.load('yingyang.png','rgb-pixel.png','blue.png','red.png', 'yellow.
           player.y = 511;
         }
 
+        //ITEM BEHAVIOUR
+        items.forEach(function(item){
 
+        if (item.collidesWith(player)) {
+          item.y = -200;
 
+          if (player.image == dogredimg && item.color == 'red')
+          {score = score + 10;}
 
-//ITEM BEHAVIOUR
-    items.forEach(function(item){
+          else if (player.image == dogyellowimg && item.color == 'yellow') {
+          score = score + 10;}
 
-      if (item.collidesWith(player)) {
-        item.y = -200;
+          else if (player.image == dogblueimg && item.color == 'blue') {
+          score = score + 10;}
 
-        if(player.image == dogredimg && item.color == 'red')
-        {score = score + 10;}
+          else {score = score - 10;}
 
-        else if(player.image == dogyellowimg && item.color == 'yellow') {
-        score = score + 10;}
-
-        else if(player.image == dogblueimg && item.color == 'blue') {
-        score = score + 10;}
-
-        else {score = score - 10;}
-
-      }
+        }
 
 
       if (item.y >= 512) {
